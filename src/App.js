08dc9884b89931fd4b52f99958f18cb305c37fa5
gregtanym <gregtanym@gmail.com>
2023-03-29 00:34:04 +0800

    deployment changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { FiSettings } from 'react-icons/fi'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
-import {Navbar, Footer, Sidebar, ThemeSettings} from './components'
+import {Navbar, Sidebar, ThemeSettings} from './components'
 import {Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Line} from './pages'
 import { useStateContext } from './contexts/ContextProvider'
 // tailwind uses shortened syntax in the classname to do the styling for u
